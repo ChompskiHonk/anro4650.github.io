753ee7210965f41fc974dd25450ea0f22f2c38cb
ChompskiHonk <77406368+ChompskiHonk@users.noreply.github.com>
2021-04-11 11:25:50 -0400

    Update myApp.js

--- freeCodeCamp/express/myApp.js
+++ freeCodeCamp/express/myApp.js
@@ -1,10 +1,10 @@
+var express = require('express');
+var app = express();
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function(req, res){
  res.send(__dirname + "/public/index.html");
 });
 console.log("Hello World");
-var express = require('express');
-var app = express();
 
 
 
