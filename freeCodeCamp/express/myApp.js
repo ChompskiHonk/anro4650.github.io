1c36255cdad99539c6dcfd440b84bd372924359e
ChompskiHonk <77406368+ChompskiHonk@users.noreply.github.com>
2021-04-14 23:27:50 -0400

    Update myApp.js

--- freeCodeCamp/express/myApp.js
+++ freeCodeCamp/express/myApp.js
@@ -1,13 +1,50 @@
+var bodyParser = require("body-parser");
 var express = require('express');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function logger (req, res, next){
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", function(req, res){
- res.send(__dirname + "/public/index.html");
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
 });
-console.log("Hello World");
-
-
-
+app.get("/:word/echo", function (req, res) {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.send({
+      time: req.time
+    });
+  }
+);
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/json", function(req, res){
+     if(process.env.MESSAGE_STYLE === "uppercase"){
+       res.json({"message" : "HELLO JSON"});
+     }else res.json({"message" : "Hello json"});
+   });
+app.post("/name", function(req, res) {
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
 
 
 
